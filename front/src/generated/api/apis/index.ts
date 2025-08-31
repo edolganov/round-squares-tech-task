da45c3a58737e2512fba14c6a30c2960dda639ce
EDolganov <evgenij.dolganov@gmail.com>
2025-08-31 07:57:14 +0300

    project files

---
+++ front/src/generated/api/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AppApi';
+export * from './AuthApi';
+export * from './RoundApi';
---
+++ front/src/generated/api/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CreateRoundReq';
+export * from './GetRoundInfoResp';
+export * from './LoginReq';
+export * from './LoginResp';
+export * from './Round';
+export * from './RoundInfo';
+export * from './TapReq';
+export * from './TapResp';
